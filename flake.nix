e95466712e7e68205210aa0e6e5b41cec3bb879f
Santiago Fraire <santiwilly@gmail.com>
2023-06-10 13:59:17 +0200

    fix: flake improvements

--- flake.nix
+++ flake.nix
@@ -3,16 +3,37 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dream2nix = {
+      url = "github:71/dream2nix/rust-ws-inherit-version";
+      inputs.crane.follows = "crane";
+    };
+    nci = {
+      url = "github:yusdacra/nix-cargo-integration";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        dream2nix.follows = "dream2nix";
+        # dream2nix.inputs.crane.follows = "crane";
+      };
+    };
+    flake-parts = {
+      inputs = {
+        nixpkgs-lib.follows = "nixpkgs";
+      };
+    };
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs = inputs@{ flake-parts, nci, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         # To import a flake module
         # 1. Add foo to inputs
         # 2. Add foo as a parameter to the outputs function
         # 3. Add here: foo.flakeModule
-
+        nci.flakeModule
       ];
       systems = [ "x86_64-linux" "aarch64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
@@ -20,14 +41,28 @@
         # module parameters provide easy access to attributes of the same
         # system.
 
-        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        packages.default = pkgs.hello;
-      };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
+        nci.projects."recipe-lang" = {
+          relPath = "";
+          # export all crates (packages and devshell) in flake outputs
+          # alternatively you can access the outputs and export them yourself
+          export = true;
+        };
 
+        # configure crates
+        nci.crates = {
+          "recp" = { };
+          "recipe-parser" = { };
+        };
+        # export the project devshell as the default devshell
+        devShells.default = config.nci.outputs."recipe-lang".devShell;
+        # export the release package of the crate as default package
+        packages.default = config.nci.outputs."recp".packages.release;
       };
+      # flake = {
+      #   # The usual flake attributes can be defined here, including system-
+      #   # agnostic ones like nixosModule and system-enumerating ones, although
+      #   # those are more easily expressed in perSystem.
+
+      # };
     };
 }
